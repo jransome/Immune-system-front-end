c96272ecb702eeddf8d09ae418ea33631f73902e
Frederick Ofori <FrederickCodes@gmail.com>
2018-01-24 14:31:21 +0000

    Spiked dropdown and state change for Criteria

--- src/components/Criteria/Criteria.js
+++ src/components/Criteria/Criteria.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class Criteria extends Component {
+    constructor(props){
+        super()
+        this.state = {
+            status: 0
+        }
+        // this.handleChange = this.handleChange.bind(this); // needed if you don't use an arrow function to create handleChange
+    }
+    // function that gets called when the selection in the dropdown changes
+    // changes the state to the new selection
+    handleChange = (event)=>{
+        this.setState({status: event.target.value})
+    }
+
     render(){
-        // console.log(this.props) // { description: 'This is a criteria.' }
         return(
             <div className="criteria">
                 { this.props.description }
+                <div className="criteria-input">
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="2" >Done</option> 
+                        <option value="1" >In Progress</option>
+                        <option value="0"  selected>Not Done</option>
+                        <option value="-1" >N/A</option>
+                    </select>
+                </div>
             </div>
         )
     }
