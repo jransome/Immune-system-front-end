0269ded3bf0c29142af79967153e8b2084561cfb
jransome <jamesransome@msn.com>
2018-02-05 10:00:35 +0000

    fix bug with selecting category and selected sub category persisting

--- src/containers/CategoryList/CategoryList.js
+++ src/containers/CategoryList/CategoryList.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectCategory } from '../../actions/CategoryActions';
+import { selectSubCategory } from '../../actions/SubCategoryActions';
 import { bindActionCreators } from 'redux';
 import { getListElements } from '../../lib/Utility';
 
 class CategoryList extends Component {
+    // TODO: Is this Reduxy enough?
+    onClickHandler = (category) => {
+        this.props.selectCategory(category);
+        this.props.selectSubCategory(null);
+    }
+
     render() {
         return (
             <ul className="list-group col-sm-4">
-                {getListElements(this.props.categories, this.props.selectCategory)}
+                {getListElements(this.props.categories, this.onClickHandler)}
             </ul>
 
         )
@@ -22,7 +29,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ selectCategory: selectCategory }, dispatch)
+    return bindActionCreators({ selectCategory: selectCategory, selectSubCategory: selectSubCategory }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CategoryList)
